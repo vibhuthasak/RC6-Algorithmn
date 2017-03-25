c1fe5b351c5c9dff3f8fbce2b14d1144375c812a
Bodhi Kumarage <vkartz@gmail.com>
2017-03-25 15:16:57 +0530

    Both key and sentence input problem fixed

--- runencrypt.m
+++ runencrypt.m
@@ -2,14 +2,15 @@ function runencrypt
 
     prompt1 = 'Enter the key(16digits)';
     key = input(prompt1,'s');
-    keygen = Keygeneration(key);
+    keyforinput = key(1:16);
+    keygen = Keygeneration(keyforinput);
     
     prompt2 = 'Enter the sentence(16digits)';
     sentence = input(prompt2,'s');
+    senforinput = sentence(1:16);
     
-    [orgi,cipher] = Encrypt(sentence,keygen);
+    [orgi,cipher] = Encrypt(senforinput,keygen);
     cell_cipher = num2cell(cipher);
-    cell_cipher
     encrypted_sentence = Deblocker(cell_cipher);
     encrypted_sentence
 
